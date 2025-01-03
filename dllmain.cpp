2256738740566f5eca2c5436fde6df53afdc7c17
lukechadwick <llukechadwick@gmail.com>
2025-01-03 17:40:21 +1100

    Handle inject and proxy methods, end thread/console on keypress

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,7 +1,9 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 
-#if PROXY
-    #include "version/version.h";
+
+// If using a proxy build config, include the necessary code to proxy calls to version.dll
+#ifdef PROXY
+    #include "version/version.h"
 #else
     #include <Windows.h>
 #endif
@@ -18,16 +20,36 @@ DWORD WINAPI MainThread(LPVOID param) {
     // Get base address from the injected process
 	uintptr_t base = (uintptr_t)GetModuleHandle(NULL);
 
-	while (true) {
+	// Allocate a console
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
 
+	// Log a message
+	std::cout << "DLL loaded successfully! Base address of the injected executable is: 0x" << std::hex << base << std::dec << std::endl;
 
-		Sleep(100);
+	// Run thread loop until END key is pressed
+	while (!GetAsyncKeyState(VK_END)) {
+		// Main thread loop
+
+		Sleep(1000);
 	}
+
+	std::cout << "Exiting..." << std::endl;
+	Sleep(1000);
+
+	HWND consoleWindow = GetConsoleWindow();
+	FreeConsole();
+	if (consoleWindow != NULL) {
+		PostMessage(consoleWindow, WM_CLOSE, 0, 0);
+	}
+
+	FreeConsole();
+	FreeLibraryAndExitThread((HMODULE)param, 0);
 	return 0;
 }
 
 
-// Main DLL entry point
 // Main DLL entry point
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -37,18 +59,10 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		// Allocate a console
-		AllocConsole();
-		FILE* f;
-		freopen_s(&f, "CONOUT$", "w", stdout);
-
-		// Log a message
-		std::cout << "DLL loaded successfully!" << std::endl;
-
-		// Handle proxy calls to version.dll
-#if PROXY
-		setupWrappers();
-#endif
+		// Handle proxy calls to version.dll, if using a proxy build config
+		#if PROXY
+			setupWrappers();
+		#endif
 
 		// Create thread
 		CreateThread(0, 0, MainThread, hModule, 0, 0);
